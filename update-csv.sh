2a4a14389a19ce66d6b989e6ee07c630ab901d17
Mathias Bynens <mathias@qiwi.be>
2021-01-06 14:56:47 +0100

    Add basic chart
    Issue: #1

--- update-csv.sh
+++ update-csv.sh
@@ -3,3 +3,4 @@
 mkdir -p tmp;
 curl --output tmp/data.xlsx 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile';
 node process-xlsx.js;
+node build-chart.js;
