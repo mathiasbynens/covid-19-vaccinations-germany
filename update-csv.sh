4955f604a71d141a06831a2a2d6f11770a6cd1e9
Mathias Bynens <mathias@qiwi.be>
2021-01-22 15:22:19 +0100

    Restore --silent flag
    Issue: #4

--- update-csv.sh
+++ update-csv.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p tmp;
-curl --dump-header - --output tmp/data.xlsx 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile';
+curl --silent --dump-header - --output tmp/data.xlsx 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile';
 node process-xlsx.js;
 node build-chart.js;
