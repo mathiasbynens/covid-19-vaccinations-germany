a832c2907b996456e93424e5ba67d870cdc262e1
Mathias Bynens <mathias@qiwi.be>
2021-03-04 12:30:20 +0100

    Support new upstream data source for deliveries

--- process-deliveries.js
+++ process-deliveries.js
@@ -21,10 +21,33 @@ const formatVaccine = (id) => {
   }
 };
 
+const stateMap = new Map([
+  ['DE-BW', 'Baden-Württemberg'],
+  ['DE-BY', 'Bayern'],
+  ['DE-BE', 'Berlin'],
+  ['DE-BB', 'Brandenburg'],
+  ['DE-HB', 'Bremen'],
+  ['DE-HH', 'Hamburg'],
+  ['DE-HE', 'Hessen'],
+  ['DE-MV', 'Mecklenburg-Vorpommern'],
+  ['DE-NI', 'Niedersachsen'],
+  ['DE-NW', 'Nordrhein-Westfalen'],
+  ['DE-RP', 'Rheinland-Pfalz'],
+  ['DE-SL', 'Saarland'],
+  ['DE-SN', 'Sachsen'],
+  ['DE-ST', 'Sachsen-Anhalt'],
+  ['DE-SH', 'Schleswig-Holstein'],
+  ['DE-TH', 'Thüringen'],
+]);
+const formatState = (id) => {
+  return stateMap.get(id);
+};
+
 const newRecords = [];
 for (const record of records) {
   newRecords.push({
     date: record.date,
+    state: formatState(record.region),
     type: formatVaccine(record.impfstoff),
     doses: record.dosen,
   });
--- update-csv.sh
+++ update-csv.sh
@@ -2,7 +2,7 @@
 
 mkdir -p tmp;
 
-curl --silent --dump-header - --output tmp/deliveries.csv 'https://impfdashboard.de/static/data/germany_deliveries_timeseries.tsv';
+curl --silent --dump-header - --output tmp/deliveries.csv 'https://impfdashboard.de/static/data/germany_deliveries_timeseries_v2.tsv';
 node process-deliveries.js;
 
 curl --silent --dump-header - --output tmp/data.xlsx 'https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.xlsx?__blob=publicationFile';
