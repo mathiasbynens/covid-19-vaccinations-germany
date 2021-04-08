9c46dc71dacaeb013fdb57237c10446e0c5dd882
Mathias Bynens <mathias@qiwi.be>
2021-04-08 23:42:37 +0200

    Support upstream changes (#18)
    Issue: #17

--- process-xlsx.js
+++ process-xlsx.js
@@ -47,7 +47,7 @@ const readPubDate = async () => {
 const readDate = async () => {
   const sheets = await readXlsxFile(PATH_TO_SPREADSHEET, { getSheets: true });
   for (const sheet of sheets) {
-    if (sheet.name.startsWith('Gesamt_bis_einschl_')) {
+    if (sheet.name.startsWith('Impfquote_bis_einschl_')) {
       return extractDate(sheet.name);
     }
   }
@@ -58,7 +58,12 @@ const PATH_TO_SPREADSHEET = './tmp/data.xlsx';
 const processRecords = (records) => {
   const data = [];
   for (const row of records.rows) {
-    if (row.state.startsWith('* ') || row.state === 'Gesamt') {
+    if (
+      row.state.startsWith('* ') ||
+      row.state.startsWith('RS: ') ||
+      row.state.startsWith('Die Daten ') ||
+      row.state === 'Gesamt'
+    ) {
       continue;
     }
     row.state = row.state.replace(/\*+$/, '').trim();
@@ -69,88 +74,107 @@ const processRecords = (records) => {
 
 // “Bund (Einsatzkräfte Bundeswehr, Bundespolizei)” is not a real
 // state, and lacks a total population count.
-const BUND = 'Bund';
+const BUND = 'Impfzentren Bund';
 
 const readMainData = async () => {
-  const records = await readXlsxFile(PATH_TO_SPREADSHEET, { sheet: 2 });
-  const headerRow = records[2];
+  const records = await readXlsxFile(PATH_TO_SPREADSHEET, { sheet: 3 });
+  const headerRow = records[3];
   for (let i = 0; i < headerRow.length; i++) {
     if (headerRow[i] === null) {
-      // If C3 is empty, re-use the contents of B3, falling back to A3.
-      headerRow[i] = records[1][i] || records[0][i];
+      // If D4 is empty, fall back to the contents of C3, B3, A3.
+      headerRow[i] = records[2][i] || records[1][i] || records[0][i];
     }
     // Ensure every header cell’s content is unique.
     headerRow[i] += `_${i}`;
   }
-  const recordsWithData = records.slice(2);
+  const recordsWithData = records.slice(3);
   const schema = {
     'Bundesland_1': {
       prop: 'state',
       type: String,
     },
-    // Gesamtzahl bisher verabreichter Impfstoffdosen
-    'Gesamtzahl bisher verabreichter Impfstoffdosen_2': {
-      prop: 'totalDosesCumulative',
+
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → eine Impfung → Impfungen kumulativ → Gesamt
+    'Gesamt_2': {
+      prop: 'firstDosesCumulativeInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Erstimpfung → Impfungen kumulativ → Gesamt
-    'Gesamt_3': {
-      prop: 'firstDosesCumulative',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → eine Impfung → Impfungen kumulativ → BioNTech
+    'BioNTech_3': {
+      prop: 'firstDosesCumulativeBioNTechInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Erstimpfung → Impfungen kumulativ → BioNTech
-    'BioNTech_4': {
-      prop: 'firstDosesCumulativeBioNTech',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → eine Impfung → Impfungen kumulativ → Moderna
+    'Moderna_4': {
+      prop: 'firstDosesCumulativeModernaInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Erstimpfung → Impfungen kumulativ → Moderna
-    'Moderna_5': {
-      prop: 'firstDosesCumulativeModerna',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → eine Impfung → Impfungen kumulativ → AstraZeneca
+    'AstraZeneca_5': {
+      prop: 'firstDosesCumulativeAstraZenecaInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Erstimpfung → Impfungen kumulativ → AstraZeneca
-    'AstraZeneca_6': {
-      prop: 'firstDosesCumulativeAstraZeneca',
+
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → vollständig geimpft → Impfungen kumulativ → Gesamt
+    'Gesamt_7': {
+      prop: 'secondDosesCumulativeInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Erstimpfung → Differenz zum Vortag
-    // 'Differenz zum Vortag_7': {
-    //   prop: 'deltaToPreviousDay',
-    //   type: Number,
-    // },
-    // Erstimpfung → Impf-quote, %
-    'Impf-quote, %_8': {
-      prop: 'firstDosesPercent',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → vollständig geimpft → Impfungen kumulativ → BioNTech
+    'BioNTech_8': {
+      prop: 'secondDosesCumulativeBioNTechInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Zweitimpfung → Impfungen kumulativ → Gesamt
-    'Gesamt_9': {
-      prop: 'secondDosesCumulative',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → vollständig geimpft → Impfungen kumulativ → Moderna
+    'Moderna_9': {
+      prop: 'secondDosesCumulativeModernaInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Zweitimpfung → Impfungen kumulativ → BioNTech
-    'BioNTech_10': {
-      prop: 'secondDosesCumulativeBioNTech',
+    // Impfungen in Impfzentren, Mobilen Teams, Krankenhäusern → vollständig geimpft → Impfungen kumulativ → AstraZeneca
+    'AstraZeneca_10': {
+      prop: 'secondDosesCumulativeAstraZenecaInCentersHospitalsMobileTeams',
       type: Number,
     },
-    // Zweitimpfung → Impfungen kumulativ → Moderna
-    'Moderna_11': {
-      prop: 'secondDosesCumulativeModerna',
+
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → Impfungen kumulativ → Gesamt
+    'Gesamt_12': {
+      prop: 'firstDosesCumulativeAtDoctors',
       type: Number,
     },
-    // Zweitimpfung → Impfungen kumulativ → AstraZeneca
-    'AstraZeneca_12': {
-      prop: 'secondDosesCumulativeAstraZeneca',
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → Impfungen kumulativ → BioNTech
+    'BioNTech_13': {
+      prop: 'firstDosesCumulativeBioNTechAtDoctors',
       type: Number,
     },
-    // // Zweitimpfung → Differenz zum Vortag
-    // 'Differenz zum Vortag_13': {
-    //   prop: 'secondDosesDeltaToPreviousDay',
-    //   type: Number,
-    // },
-    // Zweitimpfung → Impf-quote, %
-    'Impf-quote, %_14': {
-      prop: 'secondDosesPercent',
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → Impfungen kumulativ → Moderna
+    'Moderna_14': {
+      prop: 'firstDosesCumulativeModernaAtDoctors',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → Impfungen kumulativ → AstraZeneca
+    'AstraZeneca_15': {
+      prop: 'firstDosesCumulativeAstraZenecaAtDoctors',
+      type: Number,
+    },
+
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → Impfungen kumulativ → Gesamt
+    'Gesamt_17': {
+      prop: 'secondDosesCumulativeAtDoctors',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → Impfungen kumulativ → BioNTech
+    'BioNTech_18': {
+      prop: 'secondDosesCumulativeBioNTechAtDoctors',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → Impfungen kumulativ → Moderna
+    'Moderna_19': {
+      prop: 'secondDosesCumulativeModernaAtDoctors',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → Impfungen kumulativ → AstraZeneca
+    'AstraZeneca_20': {
+      prop: 'secondDosesCumulativeAstraZenecaAtDoctors',
       type: Number,
     },
   };
@@ -159,18 +183,18 @@ const readMainData = async () => {
   return data;
 };
 
-const readReasonData = async () => {
-  const records = await readXlsxFile(PATH_TO_SPREADSHEET, { sheet: 3 });
-  const headerRow = records[1];
+const readPercentData = async () => {
+  const records = await readXlsxFile(PATH_TO_SPREADSHEET, { sheet: 2 });
+  const headerRow = records[3];
   for (let i = 0; i < headerRow.length; i++) {
     if (headerRow[i] === null) {
-      // If B3 is empty, re-use the contents of A3.
-      headerRow[i] = records[0][i];
+      // If D3 is empty, fall back to the contents of C3, B3, A3.
+      headerRow[i] = records[2][i] || records[1][i] || records[0][i];
     }
     // Ensure every header cell’s content is unique.
     headerRow[i] += `_${i}`;
   }
-  const recordsWithData = records.slice(1);
+  const recordsWithData = records.slice(3);
   // Note: there can be several indications per vaccinated person
   // (e.g. an elderly person living in a nursing home with a medical
   // condition). There’s no point in summing up these numbers.
@@ -179,45 +203,83 @@ const readReasonData = async () => {
       prop: 'state',
       type: String,
     },
-    // Erstimpfung → Indikation nach Alter*
-    'Indikation nach Alter*_2': {
-      prop: 'firstDosesDueToAge',
-      type: Number,
-    },
-    // Erstimpfung → Berufliche Indikation*
-    'Berufliche Indikation*_3': {
-      prop: 'firstDosesDueToProfession',
-      type: Number,
-    },
-    // Erstimpfung → Medizinische Indikation*
-    'Medizinische Indikation*_4': {
-      prop: 'firstDosesDueToMedicalReasons',
-      type: Number,
-    },
-    // Erstimpfung → Pflegeheim-bewohnerIn*
-    'Pflegeheim-bewohnerIn*_5': {
-      prop: 'firstDosesToNursingHomeResidents',
+    // Insgesamt über alle Impfstellen → Gesamtzahl bisher verabreichter Impfungen
+    'Gesamtzahl bisher verabreichter Impfungen_2': {
+      prop: 'totalDosesCumulative',
       type: Number,
     },
 
-    // Zweitimpfung → Indikation nach Alter*
-    'Indikation nach Alter*_6': {
-      prop: 'secondDosesDueToAge',
+    // Insgesamt über alle Impfstellen → Impfquote mit einer Impfung → Gesamt
+    'Gesamt_3': {
+      prop: 'firstDosesPercent',
       type: Number,
     },
-    // Zweitimpfung → Berufliche Indikation*
-    'Berufliche Indikation*_7': {
-      prop: 'secondDosesDueToProfession',
+    // Insgesamt über alle Impfstellen → Impfquote mit einer Impfung → <60 Jahre
+    '<60 Jahre_4': {
+      prop: 'firstDosesPercentOfPeopleBelow60',
       type: Number,
     },
-    // Zweitimpfung → Medizinische Indikation*
-    'Medizinische Indikation*_8': {
-      prop: 'secondDosesDueToMedicalReasons',
+    // Insgesamt über alle Impfstellen → Impfquote mit einer Impfung → 60+ Jahre
+    '60+ Jahre_5': {
+      prop: 'firstDosesPercentOfPeopleAbove60',
       type: Number,
     },
-    // Zweitimpfung → Pflegeheim-bewohnerIn*
-    'Pflegeheim-bewohnerIn*_9': {
-      prop: 'secondDosesToNursingHomeResidents',
+
+    // Insgesamt über alle Impfstellen → Impfquote vollständig geimpft → Gesamt
+    'Gesamt_6': {
+      prop: 'secondDosesPercent',
+      type: Number,
+    },
+    // Insgesamt über alle Impfstellen → Impfquote vollständig geimpft → <60 Jahre
+    '<60 Jahre_7': {
+      prop: 'secondDosesPercentOfPeopleBelow60',
+      type: Number,
+    },
+    // Insgesamt über alle Impfstellen → Impfquote vollständig geimpft → 60+ Jahre
+    '60+ Jahre_8': {
+      prop: 'secondDosesPercentOfPeopleAbove60',
+      type: Number,
+    },
+
+    // Impfungen in Impfzentren, Mobilen Teams und Krankenhäusern → eine Impfung → <60 Jahre
+    '<60 Jahre_9': {
+      prop: 'firstDosesCumulativeInCentersHospitalsMobileTeamsForPeopleBelow60',
+      type: Number,
+    },
+    // Impfungen in Impfzentren, Mobilen Teams und Krankenhäusern → eine Impfung → 60+ Jahre
+    '60+ Jahre_10': {
+      prop: 'firstDosesCumulativeInCentersHospitalsMobileTeamsForPeopleAbove60',
+      type: Number,
+    },
+    // Impfungen in Impfzentren, Mobilen Teams und Krankenhäusern → vollständig geimpft → <60 Jahre
+    '<60 Jahre_11': {
+      prop: 'secondDosesCumulativeInCentersHospitalsMobileTeamsForPeopleBelow60',
+      type: Number,
+    },
+    // Impfungen in Impfzentren, Mobilen Teams und Krankenhäusern → vollständig geimpft → 60+ Jahre
+    '60+Jahre_12': {
+      prop: 'secondDosesCumulativeInCentersHospitalsMobileTeamsForPeopleAbove60',
+      type: Number,
+    },
+
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → <60 Jahre
+    '<60 Jahre_13': {
+      prop: 'firstDosesCumulativeAtDoctorsForPeopleBelow60',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → eine Impfung → 60+ Jahre
+    '60+ Jahre_14': {
+      prop: 'firstDosesCumulativeAtDoctorsForPeopleAbove60',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → <60 Jahre
+    '<60 Jahre_15': {
+      prop: 'secondDosesCumulativeAtDoctorsForPeopleBelow60',
+      type: Number,
+    },
+    // Impfungen bei niedergelassenen Ärzten → vollständig geimpft → 60+ Jahre
+    '60+ Jahre_16': {
+      prop: 'secondDosesCumulativeAtDoctorsForPeopleAbove60',
       type: Number,
     },
   };
@@ -241,10 +303,10 @@ const readReasonData = async () => {
     map.set(state, object);
   }
 
-  const reasonData = await readReasonData();
+  const percentData = await readPercentData();
   const result = [];
   const bundResult = [];
-  for (const object of reasonData) {
+  for (const object of percentData) {
     const state = object.state;
     const old = map.get(state);
     const isBund = state === BUND;
@@ -254,24 +316,24 @@ const readReasonData = async () => {
       date,
       pubDate,
       state,
-      firstDosesCumulative: old.firstDosesCumulative,
-      firstDosesCumulativeBioNTech: old.firstDosesCumulativeBioNTech,
-      firstDosesCumulativeModerna: old.firstDosesCumulativeModerna,
-      firstDosesCumulativeAstraZeneca: old.firstDosesCumulativeAstraZeneca,
-      firstDosesPercent: old.firstDosesPercent,
-      firstDosesDueToAge: object.firstDosesDueToAge,
-      firstDosesDueToProfession: object.firstDosesDueToProfession,
-      firstDosesDueToMedicalReasons: object.firstDosesDueToMedicalReasons,
-      firstDosesToNursingHomeResidents: object.firstDosesToNursingHomeResidents,
-      secondDosesCumulative: old.secondDosesCumulative,
-      secondDosesCumulativeBioNTech: old.secondDosesCumulativeBioNTech,
-      secondDosesCumulativeModerna: old.secondDosesCumulativeModerna,
-      secondDosesCumulativeAstraZeneca: old.secondDosesCumulativeAstraZeneca || 0,
-      secondDosesPercent: old.secondDosesPercent,
-      secondDosesDueToAge: object.secondDosesDueToAge,
-      secondDosesDueToProfession: object.secondDosesDueToProfession,
-      secondDosesDueToMedicalReasons: object.secondDosesDueToMedicalReasons,
-      secondDosesToNursingHomeResidents: object.secondDosesToNursingHomeResidents,
+      firstDosesCumulative: old.firstDosesCumulativeInCentersHospitalsMobileTeams + old.firstDosesCumulativeAtDoctors,
+      firstDosesCumulativeBioNTech: old.firstDosesCumulativeBioNTechInCentersHospitalsMobileTeams + old.firstDosesCumulativeBioNTechAtDoctors,
+      firstDosesCumulativeModerna: old.firstDosesCumulativeModernaInCentersHospitalsMobileTeams + old.firstDosesCumulativeModernaAtDoctors,
+      firstDosesCumulativeAstraZeneca: old.firstDosesCumulativeAstraZenecaInCentersHospitalsMobileTeams + old.firstDosesCumulativeAstraZenecaAtDoctors,
+      firstDosesPercent: object.firstDosesPercent,
+      firstDosesDueToAge: '',
+      firstDosesDueToProfession: '',
+      firstDosesDueToMedicalReasons: '',
+      firstDosesToNursingHomeResidents: '',
+      secondDosesCumulative: old.secondDosesCumulativeInCentersHospitalsMobileTeams + old.secondDosesCumulativeAtDoctors,
+      secondDosesCumulativeBioNTech: old.secondDosesCumulativeBioNTechInCentersHospitalsMobileTeams + old.secondDosesCumulativeBioNTechAtDoctors,
+      secondDosesCumulativeModerna: old.secondDosesCumulativeModernaInCentersHospitalsMobileTeams + old.secondDosesCumulativeModernaAtDoctors,
+      secondDosesCumulativeAstraZeneca: old.secondDosesCumulativeAstraZenecaInCentersHospitalsMobileTeams + old.secondDosesCumulativeAstraZenecaAtDoctors,
+      secondDosesPercent: object.secondDosesPercent,
+      secondDosesDueToAge: '',
+      secondDosesDueToProfession: '',
+      secondDosesDueToMedicalReasons: '',
+      secondDosesToNursingHomeResidents: '',
     };
     if (isBund) {
       delete entry.state;
@@ -282,6 +344,6 @@ const readReasonData = async () => {
   }
 
   updateCsv('./data/data.csv', result, pubDate, date);
-  updateCsv('./data/bund.csv', bundResult, pubDate, date);
+  //updateCsv('./data/bund.csv', bundResult, pubDate, date);
 
 })();
